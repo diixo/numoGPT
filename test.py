4dda30e5e56bec2e771aac982d657a31c8f3747d
diixo <diixo.com@gmail.com>
2025-01-31 05:57:01 +0200

    added BPE test_gpt2

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
 
-from numogpt.model import NewGELU
+from numogpt import bpe
+from numogpt.bpe import BPETokenizer, Encoder
+
+
+
+def test_gpt2():
+    """ test gpt-2 models encoder on 50257 """
+
+    with open("data/data.txt", "r", encoding="utf-8") as f:
+        text = f.read()
+
+    e = bpe.get_encoder()
+    r = e.encode_and_show_work(text)
+
+    print(r['tokens'])
+
+    for part in r['parts']:
+        print(part)
 
 
 def main():
-    print("Hello AI!")
+    test_gpt2()
+
 
 if __name__ == "__main__":
-    main()
+    print("Hello AI!")
+    test_gpt2()
