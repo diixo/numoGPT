88709123c2037a2a3c5fe3c5f83206c89f0c06d9
diixo <diixo.com@gmail.com>
2025-02-05 12:08:07 +0200

    text_dataset: calculated max_len

--- numogpt/text_dataset.py
+++ numogpt/text_dataset.py
@@ -13,9 +13,9 @@ def load_stopwords(file_path: str):
 
 
 # (wordacy-nn) original modified: #removed hypher
-def str_tokenize_words(s: str):
-    s = re.findall("(\.?\w[\w'\.&]*\w|\w\+*#?)", str.lower(s))
-    if s: return s
+def str_tokenize_words(s: str, stopwords=set()):
+    words = re.findall("(\.?\w[\w'\.&]*\w|\w\+*#?)", str.lower(s))
+    if words: return [w for w in words if w not in stopwords]
     return []
 
 class TextFlattenDataset(Dataset):
@@ -29,15 +29,27 @@ class TextFlattenDataset(Dataset):
 
 
     def __init__(self, path_file: str, block_size: int, stopwords_path: str=None):
-        text = None
-        stop_words = load_stopwords(stopwords_path) if stopwords_path else set()
-        with open(path_file, "r", encoding="utf-8") as f:
-            text = f.read()
-            tokens_list = str_tokenize_words(text)
-            text = " ".join([token for token in tokens_list if token not in stop_words])
 
         self.block_size = block_size
         self.encoder = get_encoder()
+        text = None
+        stopwords = load_stopwords(stopwords_path) if stopwords_path else set()
+
+        with open(path_file, "r", encoding="utf-8") as f:
+            max_len = 0
+            text = f.read()
+            tokens_list = str_tokenize_words(text, stopwords)
+
+            # determine maximum
+            for i in range(len(tokens_list) - block_size):
+                txt = " ".join(tokens_list[i : i + block_size])
+                sz = len(self.encoder.encode(txt))
+                max_len =  sz if sz > max_len else max_len
+            print("max_len=", max_len)
+
+            text = " ".join(tokens_list)
+
+
         tokens = self.encoder.encode(text)
         self.X, self.Y = self.build_dataset(tokens, block_size)
         assert(len(self.X) == len(self.Y))
